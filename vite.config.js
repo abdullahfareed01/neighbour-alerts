c2ad7bd6036878f90f7c2195d706e37384840550
Abdullah Fareed <your-email@example.com>
2026-07-18 21:24:34 +0500

    Fix asset paths for nested routes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ensures assets load correctly on Netlify
+  base: "/", // ensures assets load correctly on Netlify
 });
